ea29e87d930d00a655ca9c26f6f2c3ed5952db48
Alla Semeniuta <salladeveloper@gmail.com>
2022-11-06 19:15:59 +0300

    add preloader, add to index.html

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import FilmApiTrendFetch from './js/serviceApiFilmTrend';
 import card from './templates/card.hbs';
 import './js/pagination';
 import onSubmitQuery from './js/on-submit-query';
+import onLoadPreloaderHide from './js/preloader';
 
 const gallery = document.querySelector('.card-list');
 const btnEn = document.querySelector('#en');
 const btnUk = document.querySelector('#uk');
 const searchForm = document.querySelector('#search-form');
+
+window.addEventListener('load', onLoadPreloaderHide);
 searchForm.addEventListener('submit', onSubmitQuery);
 
 const filmApiTrendFetch = new FilmApiTrendFetch();
@@ -55,29 +58,28 @@ async function fetchApiFilms() {
 
 // const listFilms = document.querySelector(".card-list")
 
-  gallery.addEventListener('click', onCardClick);
+gallery.addEventListener('click', onCardClick);
 
+async function onCardClick(event) {
+  if (event.target.classList.contains('card-list')) {
+    return;
+  }
+  filmApiTrendFetch.idFilm = event.target.getAttribute('id');
+  console.log(filmApiTrendFetch.idFilm);
+  await fetchModalCard();
+}
 
-async function onCardClick(event) {      
-    if (event.target.classList.contains('card-list')) {    
-      return;
-    }     
-    filmApiTrendFetch.idFilm = event.target.getAttribute('id');
-    console.log(filmApiTrendFetch.idFilm); 
-    await fetchModalCard();
-} 
-
-  async function fetchModalCard() {
-    try {
-      await filmApiTrendFetch.fetchFilmCard().then(data => {
-        // const makrup = modal-card(data);
-        // console.log(data.overview);
-        console.log(data);        
-        console.log(filmApiTrendFetch.movie_id); 
-        // modalCard.innerHTML = '';
-        // modalCard.insertAdjacentHTML('beforeend', makrup);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }  
+async function fetchModalCard() {
+  try {
+    await filmApiTrendFetch.fetchFilmCard().then(data => {
+      // const makrup = modal-card(data);
+      // console.log(data.overview);
+      console.log(data);
+      console.log(filmApiTrendFetch.movie_id);
+      // modalCard.innerHTML = '';
+      // modalCard.insertAdjacentHTML('beforeend', makrup);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
