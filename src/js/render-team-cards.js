c40925d8e477fccf610d7e1419be5b172f894a17
Olga Birkovska <aiba.aki9@gmail.com>
2022-11-07 00:01:55 +0200

    Update render-team-cards.js
    testing (images issue)

--- src/js/render-team-cards.js
+++ src/js/render-team-cards.js
@@ -7,11 +7,37 @@ export default function renderTeamCards(arr) {
     const teamMarkup = footerModalCard(arr);
     teamList.innerHTML = teamMarkup;
     console.log(teamMarkup);
-
-    // const cardImageContainer = document.querySelectorAll('.footer-modal__img-container');
-    // console.log(cardImageContainer);
-    // const result = cardImageContainer.map(container => {
-    //     container.innerHTML = teamMembers.poster;
-    // })
-    // console.log(result);
 }
+
+// function testMarkup(arr) {
+//     let result = arr.map(member => {
+//         const img = member.poster;
+//         const alt = member.name;
+
+//         const IMG_MARKUP = `<img srcset="./images/footer-modal-posters/${img}.jpg 1x, ./images/footer-modal-posters/${img}@2x.jpg 2x" src="./images/footer-modal-posters/${img}.jpg" alt="${alt}" class="footer-modal__image" />`
+//         return IMG_MARKUP;
+//     });
+//     console.log(result);
+//     return result;
+// }
+
+// const shalala = testMarkup(teamMembers);
+
+// const cardImageContainer = document.querySelectorAll('.footer-modal__img-container');
+//         console.log(cardImageContainer);
+//         function testttt(item) {
+//             cardImageContainer.map(container => {
+//                 container.innerHTML = item;
+//             })
+//         }
+
+// function test2(arr) {
+//     let result = arr.map(item => {
+//         testttt(item);
+//         console.log(result);
+//         } 
+//     )
+//     return result;
+// }
+
+// test2(shalala);
