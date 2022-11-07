8d2df93e3b8da0530ca50ea0e34fe4c5d00cd4be
Olga Birkovska <aiba.aki9@gmail.com>
2022-11-08 00:04:23 +0200

    добавлено закрытие модалки по клику на бекдроп и по нажатию esc

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -5,16 +5,36 @@ const footerModal = document.querySelector('.js-footer-modal');
 const footerModalOpenBtn = document.querySelector('.js-footer-modal-open');
 const footerModalCloseBtn = document.querySelector('.js-footer-modal-close');
 
-footerModalOpenBtn.addEventListener('click', onModalOpenBtnClick);
-footerModalCloseBtn.addEventListener('click', onModalCloseBtnClick);
+footerModalOpenBtn.addEventListener('click', onFooterModalOpen);
+footerModalCloseBtn.addEventListener('click', onFooterModalClose);
 
-function onModalOpenBtnClick(e) {
+function onFooterModalOpen(e) {
     renderTeamCards(teamMembers);
+
     footerModal.classList.remove('is-hidden');
     document.body.classList.add('footer-modal-opened');
+
+    footerModal.addEventListener('click', onBackdropClick);
+    window.addEventListener('keydown', onEscape);
 }
 
-function onModalCloseBtnClick(e) {
+function onFooterModalClose(e) {
     footerModal.classList.add('is-hidden');
     document.body.classList.remove('footer-modal-opened');
+
+    footerModal.removeEventListener('click', onBackdropClick);
+}
+
+function onBackdropClick(e) {
+    if (e.target !== e.currentTarget) {
+        return;
+    }
+    onFooterModalClose();
+}
+
+function onEscape(e) {
+    const ESCAPE_KEY_CODE = 'Escape';
+    if (e.code === ESCAPE_KEY_CODE) {
+        onFooterModalClose();
+    }
 }
