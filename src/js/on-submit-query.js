76e05f946b01f18a00f921bf844743ab8007a469
Alla Semeniuta <salladeveloper@gmail.com>
2022-11-09 01:39:50 +0300

    bugfix

--- src/js/on-submit-query.js
+++ src/js/on-submit-query.js
@@ -1,6 +1,6 @@
 import renderCards from './render-cards';
 import Notiflix from 'notiflix';
-import { onErrorEN, onErrorUK} from './on-error';
+import { onErrorEN, onErrorUK } from './on-error';
 import { spinnerOff, spinnerOn } from './preloader';
 
 export default async function onSubmitQuery(evt, instance) {
@@ -9,6 +9,14 @@ export default async function onSubmitQuery(evt, instance) {
   const searchQuery = evt.target.elements.searchQuery.value
     .trim()
     .toLowerCase();
+  if (searchQuery === '') {
+    spinnerOff();
+    if (instance.currentLang === 'en-US') {
+      return Notiflix.Notify.failure(`Please put some query!`);
+    } else {
+      return Notiflix.Notify.failure(`Введіть запит!`);
+    }
+  }
   instance.query = searchQuery;
   // console.log(instance.query);
   try {
@@ -18,16 +26,24 @@ export default async function onSubmitQuery(evt, instance) {
       // gallery.innerHTML = '';
       // тимчасово, далі буде перевірка на мову
       spinnerOff();
-      return Notiflix.Notify.failure(
-        `The films you requested could not be found!`
-      );
+      if (instance.currentLang === 'en-US') {
+        return Notiflix.Notify.failure(
+          `The films you requested could not be found!`
+        );
+      } else {
+        return Notiflix.Notify.failure(`За вашим запитом фільмів не знайдено!`);
+      }
     }
     // console.log(data.films);
     spinnerOff();
     renderCards(data.films);
-
-    // тимчасово, далі буде перевірка на мову
-    Notiflix.Notify.success(`We found ${data.total_results} films.`);
+    if (instance.currentLang === 'en-US') {
+      return Notiflix.Notify.success(`We found ${data.total_results} films.`);
+    } else {
+      return Notiflix.Notify.success(
+        `Ми знайшли ${data.total_results} фільмів.`
+      );
+    }
   } catch (error) {
     if (instance.currentLang === 'en-US') {
       onErrorEN();
