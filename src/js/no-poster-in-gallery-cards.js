fbc62170ceff7a1ddef045db85998bc0cb0ebdaa
Alla Semeniuta <salladeveloper@gmail.com>
2022-11-09 20:44:43 +0300

    remove fn

--- src/js/no-poster-in-gallery-cards.js
+++
@@ -1,9 +0,0 @@
-export default function noPosterCard() {
-  const noPosterCards = document.querySelectorAll(
-    "img[src='https://image.tmdb.org/t/p/w500']"
-  );
-
-  for (const card of noPosterCards) {
-    card.className = 'visually-hidden';
-  }
-}
