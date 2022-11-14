61bd131e18668ae62e693513b9451e9089326d10
Maksym Solomko <max.exc7@gmail.com>
2022-11-14 12:03:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -290,9 +290,8 @@ async function renderYear(year) {
   renderCards(await filmsAndGenres(await fetchWithYers(year)));
 }
 
-upcomingList.addEventListener('click', onCardClick);
-
 async function fetchUpcomingFilms() {
+  upcomingList.addEventListener('click', onCardClick);
   try {
     await filmApiTrendFetch.fetchUpcomingFilms().then(data => {
       const makrup = data;
@@ -319,6 +318,16 @@ async function onCardClick(event) {
     spinnerOff();
     return;
   }
+
+  async function noPosterCard() {
+    const noPosterCards = document.querySelector(
+      "img[class='movie-poster']"
+    ); 
+    for (const card of noPosterCards) {
+      card.className = 'visually-hidden';
+    }
+  }
+
   filmApiTrendFetch.idFilm = event.target.getAttribute('data-film');
   const list = document.getElementById(filmApiTrendFetch.idFilm).dataset.list;
   console.log('Это data-film:', filmApiTrendFetch.idFilm, list);
@@ -328,16 +337,7 @@ async function onCardClick(event) {
     if (e.key === 'Escape' || e.key === 'Esc') {
       await closeModal();
     }
-  };
-
-  // async function noPosterCard() {
-  //   const noPosterCards = document.querySelectorAll(
-  //     "img[class='movie-poster']"
-  //   ); 
-  //   for (const card of noPosterCards) {
-  //     card.className = 'visually-hidden';
-  //   }
-  // }
+  }; 
 
   await openModal();
 
@@ -352,7 +352,7 @@ async function onCardClick(event) {
         console.log(filmApiTrendFetch.movie_id);
         modalCard.innerHTML = '';
         modalCard.insertAdjacentHTML('beforeend', markup);        
-        // noPosterCard();
+        noPosterCard();
         spinnerOff();
         if (uid === "guest") {
           document.querySelector('.button-queue').disabled = "true";
